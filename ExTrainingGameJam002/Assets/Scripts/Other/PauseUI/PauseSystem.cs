ecd9e45425cf473a4c643c534cafe6b2a9eccaee
Shunya Miyamoto <krnmeradrs@gmail.com>
2023-07-30 01:09:35 +0900

    Create : Timer & GameOver.GamerClearUI

--- ExTrainingGameJam002/Assets/Scripts/Other/PauseUI/PauseSystem.cs
+++ ExTrainingGameJam002/Assets/Scripts/Other/PauseUI/PauseSystem.cs
@@ -7,21 +7,32 @@ namespace UISystem
     public class PauseSystem : MonoBehaviour
     {
         [SerializeField] private GameObject pauseUI;
+        [SerializeField] private GameObject GameOverUI;
 
         private void Start()
         {
+            Time.timeScale = 1f;
             pauseUI.SetActive(false);
+            GameOverUI.SetActive(false);
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (GameOverUI.activeSelf) return;
+
             if(Input.GetKeyDown("p"))
             {
                 pauseUI.SetActive(!pauseUI.activeSelf);
 
                 Time.timeScale = pauseUI.activeSelf ? 0f : 1f;
             }
+        }
+
+        public void GameOver()
+        {
+            Time.timeScale = 0f;
+            GameOverUI.SetActive(true);
         }
     }
 }
