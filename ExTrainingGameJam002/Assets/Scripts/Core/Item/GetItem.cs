8e3558e39c2d6b0676e7a35f5e435dc74fab6e17
LMN <lmnminnie@yahoo.com.hk>
2023-07-31 13:49:35 +0900

    get item - destroy script

--- ExTrainingGameJam002/Assets/Scripts/Core/Item/GetItem.cs
+++ ExTrainingGameJam002/Assets/Scripts/Core/Item/GetItem.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GetItem : MonoBehaviour
 {
-
+    public int num;
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            switch(num)
+            {
+                case 0:
+                    Debug.Log("key");
+                    break;
+                case 1:
+                    Debug.Log("poision");
+                    break;
+            }
             Destroy(gameObject);
         }
     }
